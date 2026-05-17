c0d9080c55ed973abf1e9e1415b4b850f0cde5c1
duliodelgado <dulio.delgado@gmail.com>
2026-05-16 21:03:14 -0500

    coolpics

---
+++ coolpics/script.js
@@ -0,0 +1,41 @@
+// Menu responsive
+
+const menuBtn = document.querySelector(".menu-button");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+});
+// Modal images
+const gallery = document.querySelector(".gallery");
+const modal = document.querySelector("dialog");
+const modalImg = modal.querySelector("img");
+const closeBtn = modal.querySelector(".close-viewer");
+
+gallery.addEventListener("click", (e) => {
+  if (e.target.tagName !== "IMG") return;
+
+  const src = e.target.getAttribute("src");
+  const alt = e.target.getAttribute("alt");
+
+  const full = src.replace("sm", "full");
+
+  modalImg.src = full;
+  modalImg.alt = alt;
+
+  modal.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.close();
+  }
+});
